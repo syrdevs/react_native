f255b5906c7902edde79958efe2afc8d60b1cebb
syrdevs <syrdevs@gmail.com>
2018-09-12 13:08:38 +0600

    test rest api

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+/*const logger = require('morgan');
+const mongoose = require('mongoose');*/
 const bodyParser = require('body-parser');
-const path = require('path');
+/*const path = require('path');*/
 //mongoose.connect("mongodb://127.0.0.1:27017/mean_lessons");
 
 const app = express();
 // show status and console log in terminal
-app.use(logger('dev'));
+//app.use(logger('dev'));
 // jsons to req.bodymodule
 app.use(bodyParser.urlencoded({limit:'50mb', extended: true}));
 //max size of requests
@@ -17,9 +17,9 @@ app.get('/api/hello',(req,res) => {
     res.status(200).send({msg:"WORK!"});
 })
 
-app.use(express.static(path.join(__dirname,"client/build"),{maxAge:1}))
+//app.use(express.static(path.join(__dirname,"client/build"),{maxAge:1}))
 
-app.use(require("./server/routes"));
+//app.use(require("./server/routes"));
 
 
 app.listen(3001, ()=>console.log("Server on port 3000"));
